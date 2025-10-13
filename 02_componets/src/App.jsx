f78b14784255b6898591fd584ead5e28ea53cd9e
Uzair <programmeruzair@gmail.com>
2025-10-13 16:43:42 +0530

    Components in React are like Lego blocks for your UI. You build small, reusable pieces and combine them to make the whole app. Makes coding cleaner and easier to manage.

---
+++ 02_componets/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/card"
+
+function App() {
+  return (
+    <div>
+      
+      <Card /> 
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+  // made a separate component for Card, now using it here.. it helps in reusability.. just import and use it
+}
+
+export default App
---
+++ 02_componets/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+  return (
+    <div className="Card">
+        <h1>Uzair</h1>
+        <h2>Learning React</h2>
+    </div>
+  )
+}
+
+// dividing components into different files is a good practice
+
+export default Card
